c1dc16eb8bede6fc5982df8498deecd37dbfaa04
elikrantz <87043586+elikrantz@users.noreply.github.com>
2023-03-16 22:41:51 -0400

    testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+
+public class Robot {
+    DriveController driveController;
+    BNO055IMU imu;
+    Telemetry telemetry;
+    HardwareMap hardwareMap;
+    OpMode opMode;
+
+    public Robot (OpMode opMode, boolean isAuto) {
+        this.hardwareMap = opMode.hardwareMap;
+        this.telemetry = opMode.telemetry;
+        this.opMode = opMode;
+        driveController = new DriveController(this);
+        imu = opMode.hardwareMap.get(BNO055IMU.class, "imu");
+        //imu = opMode.hardwareMap.get(IMU.class, "imu");
+    }
+
+
+    public void initIMU () {
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        //IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                //RevHubOrientationOnRobot.LogoFacingDirection.BACK,
+                //RevHubOrientationOnRobot.UsbFacingDirection.UP));
+
+        imu.initialize(parameters);
+    }
+
+    public Angle getRobotHeading () {
+        //heading is of NEG_180_TO_180_HEADING type by default (no need for conversion)
+        double heading = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+        //double heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+        return new Angle(-heading, Angle.AngleType.NEG_180_TO_180_HEADING);
+    }
+
+    public void wait (int millis, LinearOpMode linearOpMode) {
+        long startTime = System.currentTimeMillis();
+        while (millis > System.currentTimeMillis() - startTime && linearOpMode.opModeIsActive()) {}
+    }
+}
